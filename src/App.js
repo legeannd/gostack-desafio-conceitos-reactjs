435f4187f4f9237f879350c4d7d074dbb99f457f
Gean Lucas <gean.lfpsa@gmail.com>
2020-06-18 23:24:44 -0300

    Comentários nas funções

--- src/App.js
+++ src/App.js
@@ -6,12 +6,16 @@ import "./styles.css";
 function App() {
   const [repositories, setRepositories] = useState([]);
 
+  //Função useEffect é utilizada para carregar os dados da API pela primeira vez 
+
   useEffect(() => {
     api.get('repositories').then(response => {
       setRepositories(response.data);
     })
   }, []);
 
+  //A função handleAddRepository manda pro backend o objeto com os dados necessários pra criação do repositório
+  //Quando o método post retorna uma resposta, ela é usada para atualizar o frontend com o novo repositório adicionado
 
   async function handleAddRepository() {
 
@@ -26,6 +30,9 @@ function App() {
     
   }
 
+  //A função handleRemoveRepository recebe o id do repositório e manda para o backend através da rota delete
+  //Quando o objeto é removido, ele é buscado no array de repositórios que está no frontend, para ser removido e a tela ser atualizada
+
   async function handleRemoveRepository(id) {
     
     await api.delete(`repositories/${id}`);
@@ -33,19 +40,19 @@ function App() {
     setRepositories(repositories.filter(repository => repository.id !== id));
   }
 
+  //Cada <li> possui um botão para remover o repositório, e é gerado automaticamente conforme o botão de adicionar vai sendo clicado
+
   return (
     <div>
       <ul data-testid="repository-list">
         {repositories.map(repository => 
         <li key={repository.id}>
           {repository.title}
-          
           <button onClick={() => handleRemoveRepository(repository.id)}>
             Remover
           </button>
         </li>)}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
